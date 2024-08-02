570bf47c74294deb28265768b08b5fc5c8fcdd5a
dunkeyyfong <phongtrong865@gmail.com>
2024-08-03 00:28:13 +0700

    Upload

--- bypassfn/ViewController.m
+++ bypassfn/ViewController.m
@@ -6,9 +6,10 @@
 //
 
 #import "ViewController.h"
+#include <stdio.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *runCommandButton;
 @end
 
 @implementation ViewController
@@ -16,7 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
 }
 
+- (IBAction)runCommandButtonTapped:(id)sender {
+    [self runCommand:@"cd /; ls -l"];
+}
+
+- (void)runCommand:(NSString *)command {
+    const char *cmd = [command UTF8String];
+    FILE *pipe = popen(cmd, "r");
+    if (pipe == NULL) {
+        NSLog(@"Error opening pipe");
+        
+        return;
+    }
+    
+    char buffer[128];
+    NSMutableString *output = [NSMutableString string];
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        [output appendString:[NSString stringWithUTF8String:buffer]];
+    }
+    
+    pclose(pipe);
+    NSLog(@"Command output: %@", output);
+}
 
 @end
